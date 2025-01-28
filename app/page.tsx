b0632a9bbd5a476d1cbd7060cc65fcf1a1807762
HunterKTF <jorgericardo98@hotmail.com>
2025-01-28 17:33:38 -0600

    added main layout and image tile

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,52 @@
 import Image from "next/image";
+import background from "../public/pxfuel.jpg";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="w-screen h-screen p-[45px] bg-neutral-100">
+      <main className="h-full grid grid-flow-row grid-rows-10 grid-cols-10 gap-[15px]">
+        {/* Image tile */}
+        <div className="col-span-3 row-span-3 rounded-lg flex justify-end items-end px-2"
+          style={{
+            backgroundImage: `url(${background.src})`,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat'
+          }}>
+            <Image src={"/memoji-salute-2.png"} alt="Memoji saluting" width={245} height={100}/>
         </div>
+
+        {/* About tile */}
+        <div className="border border-black col-span-2 row-span-3 rounded-lg"></div>
+
+        {/* Selected Works tile */}
+        <div className="border border-black col-span-3 row-span-4 rounded-lg"></div>
+
+        {/* Interests tile */}
+        <div className="border border-black col-span-2 row-span-4 rounded-lg"></div>
+
+        {/* About Me & Links tile */}
+        <div className="border border-black col-span-5 row-span-4 rounded-lg"></div>
+
+        {/* Certifications tile */}
+        <div className="border border-black col-span-2 row-span-3 rounded-lg"></div>
+
+        {/* Skills tile */}
+        <div className="border border-black col-span-3 row-span-3 rounded-lg"></div>
+
+        {/* Hobbies tile */}
+        <div className="border border-black col-span-2 row-span-3 rounded-lg"></div>
+
+        {/* Tech Stack tile */}
+        <div className="border border-black col-span-5 row-span-2 rounded-lg"></div>
+
+        {/* Work Experience tile */}
+        <div className="border border-black col-span-3 row-span-3 rounded-lg"></div>
+
+        {/* Contact Info tile */}
+        <div className="border border-black col-span-5 row-span-1 rounded-lg"></div>
+
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
